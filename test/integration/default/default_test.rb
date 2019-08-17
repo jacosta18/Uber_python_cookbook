b9d2299140f83e3022a362fa6233c38289b9d491
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-08-17 21:20:45 +0100

    adding integration tests

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,16 +1,27 @@
-# InSpec test for recipe python::default
-
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
-
 unless os.windows?
   # This is an example test, replace with your own test.
   describe user('root'), :skip do
     it { should exist }
   end
 end
-
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+
+describe package('python') do
+  it { should be_installed }
+end
+
+describe service('python') do
+  it { should be_running }
+end
+
+describe service('python') do
+  it { should be_enabled }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe http('http://localhost',enable_remote_worker: true) do
+  its('status'){ should cmp 502 }
 end
